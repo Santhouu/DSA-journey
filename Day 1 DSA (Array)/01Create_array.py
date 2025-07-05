c3e15b953b839215d9f1ab7313a1df85ac680153
Santhosh S <santhu.santhu7788@gmail.com>
2025-07-05 14:35:51 +0530

    Update 01Create_array.py

--- Day 1 DSA (Array)/01Create_array.py
+++ Day 1 DSA (Array)/01Create_array.py
@@ -1,5 +1,5 @@
 # Array is a collection of 'Homogenous' data type of elements.
-# But in Python List also act as array but List can hold 'Heterogenous' 
+# But in Python List also act as array but List can hold 'Heterogenous' elements.
 # Creating an array
 arr = [10, 20, 30, 40, 50]
 
