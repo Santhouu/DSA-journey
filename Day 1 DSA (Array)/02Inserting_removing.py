d30b62079423dd66425cac6dc8f95e13ca4037e2
Santhosh S <santhu.santhu7788@gmail.com>
2025-07-08 23:21:50 +0530

    Update 02Inserting_removing.py

--- Day 1 DSA (Array)/02Inserting_removing.py
+++ Day 1 DSA (Array)/02Inserting_removing.py
@@ -11,6 +11,7 @@ arr.insert(2, 25)      # Add 25 at index 2
 print(arr)
 
 # Deletion
+# Pop :- Removes the end element
 arr.pop()              # Removes last element
 arr.remove(25)         # Removes value 25
 print(arr)
